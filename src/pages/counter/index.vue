cb52ba2add38e6597dee6afa87b02187a34fe37e
heqingchang <heqc@udiannet.com>
2018-03-11 22:41:11 +0800

    test: 提交未提交内容

--- src/pages/counter/index.vue
+++ src/pages/counter/index.vue
@@ -23,6 +23,13 @@
         
       </div>
     </div>
+    <div class="map-test">
+      <!-- <map id="theMap" style="width: 100%; height:100%" longitude='114.05956' latitude='22.54286' scale='12'/> -->
+
+      <map id="theMap" style="width: 100%; height:100%" :longitude='longitude' :latitude='latitude' scale='12' :markers='markers' @markertap='onMarkerTap' :polyline="polyline" />
+      <canvas></canvas>
+    </div>
+    
     <!-- <p>Vuex counter：{{ count }}</p>
     <p>
       <button @click="increment">+</button>
@@ -38,10 +45,72 @@
 import store from './store'
 
 export default {
+  data () {
+    return {
+      longitude: '',
+      latitude: '',
+      polyline: '',
+      markers: []
+    }
+  },
   computed: {
     count () {
       return store.state.count
     }
+  },
+  created () {
+    console.log(wx)
+    const _this = this
+    wx.getLocation({
+      type: 'gcj02',
+      success (res) {
+        console.log(res)
+        _this.longitude = res.longitude
+        _this.latitude = res.latitude
+        // wx.openLocation({
+        //   longitude: res.longitude,
+        //   latitude: res.latitude,
+        //   scale: 18
+        // })
+        _this.markers.push({
+          id: 1,
+          latitude: res.latitude,
+          longitude: res.longitude,
+          title: 'testMarker',
+          iconPath: '/static/icon_pin_red2x.png',
+          callout: {
+            content: 'testtest',
+            color: '#900',
+            fontSize: 30,
+            bgColor: '#090',
+            padding: 30,
+            display: 'ALWAYS',
+            textAlign: 'center'
+          },
+          label: {
+            content: 'test11111'
+          },
+          polyline: {
+            points: [
+              {
+                longitude: res.longitude,
+                latitude: res.latitude
+              },
+              {
+                longitude: res.longitude - 0.000001,
+                latitude: res.latitude - 0.000001
+              }
+            ],
+            color: '#000',
+            arrowLine: true,
+            borderWidth: 20
+          }
+        })
+      },
+      fail (res) {
+        console.log(res)
+      }
+    })
   },
   methods: {
     increment () {
@@ -49,16 +118,29 @@ export default {
     },
     decrement () {
       store.commit('decrement')
+    },
+    onMarkerTap () {
+      console.log(1)
     }
   }
 }
 
 </script>
 <style lang="stylus">
+page {
+  width: 100%;
+  height: 100%;
+}
 .counter-wrap {
+  width 100%
+  height: 100%
   background-color #900
   .input-holder {
     background-color #090
+  }
+  .map-test {
+    width: 100%
+    height: 100%
   }
 }
 
